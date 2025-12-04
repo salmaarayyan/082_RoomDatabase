27462c111eb910919b3e287addfd4b652e1f71c1
salmaarayyan <salmaarayyan@gmail.com>
2025-12-04 11:31:56 +0700

    Membuat destinasi untuk halaman detail siswa

---
+++ app/src/main/java/com/example/a20230140082_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.a20230140082_roomdatabase.view.route
+
+import com.example.a20230140082_roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
